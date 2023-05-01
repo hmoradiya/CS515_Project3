e2c4a93f21ed566c60de34fa35b6dfdabdfdf2af
hardiksrashtasoft <hardikm.srashtasoft@gmail.com>
2023-05-01 01:17:33 -0400

    remove files

--- test.sh
+++ test.sh
@@ -7,5 +7,3 @@ trap 'kill $PID' EXIT # kill the server on exit
 PID=$! # record the PID
 
 newman run Forum.postman_collection.json -e env.json # use the env file
-newman run forum_multiple_posts.postman_collection.json -e env.json # use the env file
-newman run forum_post_read_delete.postman_collection.json -n 50 # 50 iterations
